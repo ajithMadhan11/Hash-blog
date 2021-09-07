4d2ca3bd505a75dfe113dde601cb79319f0aafcf
Ajith madhan <ajithmadhan.dev113@gmail.com>
2021-09-07 22:14:39 +0530

    redux-persistent configured

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Routes from './Routes';
+import React from "react";
+import ReactDOM from "react-dom";
+import Routes from "./Routes";
 import { Provider } from "react-redux";
+import { persistStore } from "redux-persist";
 import store from "./store/index";
+import { PersistGate } from "redux-persist/integration/react";
+import Loader from "./Components/Loader";
+
+let persistor = persistStore(store);
 
 ReactDOM.render(
   <Provider store={store}>
-  <Routes />
-</Provider>
-  ,document.getElementById('root')
+    <PersistGate loading={<Loader />} persistor={persistor}>
+      <Routes />
+    </PersistGate>
+  </Provider>,
+  document.getElementById("root")
 );
-
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,35 @@
-import { createStore } from "redux";
+import { createStore, compose } from "redux";
 import rootReducer from "../reducer";
+import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { encryptTransform } from "redux-persist-transform-encrypt";
 
-const store = createStore(rootReducer);
+const persistConfig = {
+  key: "root",
+  storage,
+  stateReconciler: autoMergeLevel2,
+  transforms: [
+    encryptTransform({
+      secretKey: "aspirine",
+      onError: function (error) {
+        console.log(error);
+      },
+    }),
+  ],
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
+const composeEnhancers =
+  (typeof window !== "undefined" &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const store = createStore(persistedReducer, composeEnhancers());
+
+// export default () => {
+//     let store = createStore(persistedReducer)
+//     let persistor = persistStore(store)
+//     return { store, persistor }
+//   }
 export default store;
